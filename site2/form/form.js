dd31bea53115e6b3ac4bbafa8ab1bfa90e69e87d
a <a>
2024-09-03 16:32:51 -0300

    projeto3

--- site2/form/form.js
+++ site2/form/form.js
@@ -1,28 +1,28 @@
-const url = "http://localhost:3000/pecas"
+const url = "http://localhost:3000/pecas";
 
-document.getElementById('dataForm').addEventListener('submit', function(e){
-    e.preventDefault();
+document.getElementById("dataForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const dataForm = {
-        nome: document.getElementById('nome').value,
-        preco: document.getElementById('preco').value,
-        precoNew: document.getElementById('precoNew').value,
-        quantidade: document.getElementById('quantidade').value,
-        url: document.getElementById('url').value
-    };
+  const dataForm = {
+    nome: document.getElementById("nome").value,
+    preco: document.getElementById("preco").value,
+    precoNew: document.getElementById("precoNew").value,
+    quantidade: document.getElementById("quantidade").value,
+    url: document.getElementById("url").value,
+  };
 
-    fetch('http://localhost:3000/pecas', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(dataForm),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('success:',data);
+  fetch("http://localhost:3000/pecas", {
+    method: "POST", // que estou enviando dados para API
+    // headers: {
+    //     'Content-Type': 'application/json',
+    // },
+    body: JSON.stringify(dataForm), // que as informaçoes estão vindo do corpo (nomalmente um formulario)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("success:", data);
     })
     .catch((error) => {
-        console.error('Error:', error);
+      console.error("Error:", error);
     });
 });
